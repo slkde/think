a9eca3cc2872323dbc74c2f2cf3ba48f75ab78c3
kangkangguo <1558573029@qq.com>
2018-05-12 10:38:42 +0800

    111

---
+++ runtime/temp/5fcebd9643c698ba94cdae0bf99e9e8d.php
@@ -0,0 +1,14 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:71:"G:\phpStudy\WWW\think\public/../application/index\view\index\index.html";i:1526092020;}*/ ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>郭康康</p>
+</body>
+</html>

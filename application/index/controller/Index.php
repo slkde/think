3f168ab495e8826a4295564b10fb603ae4e6fe7c
kangkangguo <1558573029@qq.com>
2018-05-12 18:40:45 +0800

    333

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,15 +6,23 @@ use think\Url;
 use think\Db;
 use think\Session; 
 
-class Index  extends Common 
+//header("Content-type:text/html;charset=UTF-8");
+header("Content-type:text/html;charset=utf-8");
+class Index  extends Common
 {
 
 	/*
-	* PC端网站首页
+	* 博客网站首页
 	*/
 	public function index()
-	{  
-		echo "123";
+	{
+	    $data = Db::name('article')->where('is_display',1)->select();
+        foreach($data as $key=>&$value){
+            $value['cate_name'] = Db::name('category')->where('cate_id',$value['cate_id'])->value('cate_name');
+//            var_dump($value);die;
+        }
+//        var_dump($data);die;
+        $this->assign('data',$data);
 		return view();
 	}
      
